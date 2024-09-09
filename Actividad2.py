de2543fb93be7dfccef266317e7dafe04ee9ff42
mamir7 <mirkasantos5@gmail.com>
2024-09-09 13:56:40 -0600

    Dibujar triangulo

--- Actividad2.py
+++ Actividad2.py
@@ -9,10 +9,14 @@ Exercises
 """
 
 from random import randrange
+
 from turtle import *
 
 from freegames import square, vector
 
+from random import randrange, choice
+
+pick_random_color = lambda: choice(["green", "blue", "orange", "pink", 'purple'])
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
