247ddb0d4742eb4489b794e607ee43debc1d13b2
Diego Javier Solorzano Trinidad <111060171+Diegosoltri@users.noreply.github.com>
2024-09-10 13:05:21 -0600

    Update Actividad5.py

--- Actividad5.py
+++ Actividad5.py
@@ -65,9 +65,9 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x + 25, y)
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        write(tiles[mark],align="center",font=('Arial', 30, 'normal'))
 
     # Dibujar el contador en la esquina superior izquierda
     up()
