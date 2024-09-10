7015d57e8112eed9c82805f70c2e6936e483a508
danielcastillo52 <A01737357@tec.mx>
2024-09-10 12:58:25 -0600

    Update Actividad5.py

--- Actividad5.py
+++ Actividad5.py
@@ -1,26 +1,12 @@
-"""Memory, puzzle game of number pairs.
-
-Exercises:
-
-1. Count and print how many taps occur.
-2. Decrease the number of tiles to a 4x4 grid.
-3. Detect when all tiles are revealed.
-4. Center single-digit tile.
-5. Use letters instead of tiles.
-"""
-
-from random import *
+from random import shuffle
 from turtle import *
-
 from freegames import path
 
 car = path('car.gif')
-"""List of symbols instead of numbers"""
-symbols = ['@', '#', '£', '∞', '$', '%', '&', '!', '*', '+', '-', '/', '?', '§', '♥', '♦', '♣', '♠', '©', '®', '✓', '★', '☺', '☻', '☼', '☽', '⚽', '⚾', '♫', '♪', '♥', '♦']
-tiles = symbols * 2
+tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
-
+tap_count = 0  # Contador de taps
 
 def square(x, y):
     """Draw white square with black outline at (x, y)."""
@@ -34,19 +20,17 @@ def square(x, y):
         left(90)
     end_fill()
 
-
 def index(x, y):
     """Convert (x, y) coordinates to tiles index."""
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
-
 
 def xy(count):
     """Convert tiles count to (x, y) coordinates."""
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
-
 def tap(x, y):
     """Update mark and hidden tiles based on tap."""
+    global tap_count  # Necesario para modificar la variable global
     spot = index(x, y)
     mark = state['mark']
 
@@ -56,7 +40,11 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
-
+    
+    tap_count += 1  # Incrementar el contador cada vez que se hace un tap
+    
+def revealed():
+    return all(not h for h in hide)
 
 def draw():
     """Draw image and tiles."""
@@ -75,16 +63,26 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 25, y)
+        goto(x + 2, y)
         color('black')
-        write(tiles[mark],align="center",font=('Arial', 30, 'normal'))
+        write(tiles[mark], font=('Arial', 30, 'normal'))
 
+    # Dibujar el contador en la esquina superior izquierda
+    up()
+    goto(-200, 200)
+    color('black')
+    write(f"Contador: {tap_count}", align="left", font=("Arial", 12, "normal"))
+
+    if revealed():
+        up()
+        goto(-200, -250)
+        write("HAS DESTAPADO TODOS LOS CUADROS", align="left", font=("Arial", 14, "bold"))
+        
     update()
     ontimer(draw, 100)
 
-
 shuffle(tiles)
-setup(420, 420, 370, 0)
+setup(420, 550, 370, 0)
 addshape(car)
 hideturtle()
 tracer(False)
