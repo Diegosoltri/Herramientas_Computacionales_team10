00e015ce80c41378c5719f54d0a91f716d511ccc
mamir7 <mirkasantos5@gmail.com>
2024-09-10 11:38:57 -0600

    Dibujar triangulo

--- Actividad3.py
+++ Actividad3.py
@@ -132,7 +132,7 @@ def move():
 
     for point, course in ghosts:
         if valid(point + course):
-            point.move(course)
+            point.move(course*4)
         else:
             options = [
                 vector(5, 0),
